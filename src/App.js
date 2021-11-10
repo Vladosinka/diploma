786188c20bf1cb14d8458efc02a8ba7d00969b59
Vladosinka <Vladshumaher1999@gmail.com>
2021-11-11 00:20:36 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = (props) => {
   return ( 
@@ -17,13 +17,11 @@ const App = (props) => {
           <div className = 'flex'>
             <Navbar / >
               <div className = 'app-wrapper-fon'>
-                <Routes>
-                  <Route path="/profile" element={<Profile/>}/>
-                  <Route path="/dialogs" element={<Dialogs/>}/>
-                  <Route path="/feed" element={<News/>}/>
-                  <Route path="music/" element={<Music/>}/>
-                  <Route path="/settings" element={<Settings/>}/>
-                </Routes>
+                  <Route path="/profile" component={Profile}/>
+                  <Route path="/dialogs" component={Dialogs}/>
+                  <Route path="/feed" component={News}/>
+                  <Route path="/music" component={Music}/>
+                  <Route path="/settings" component={Settings}/>
               </div>
           </div>
       </div>
