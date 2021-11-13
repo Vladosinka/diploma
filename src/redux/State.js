060c903ac5033eb36069911195bfa8af1f32dc12
Vladosinka <Vladshumaher1999@gmail.com>
2021-11-13 14:01:16 +0300

    problem "alt prop" solved

--- src/redux/State.js
+++ src/redux/State.js
@@ -12,41 +12,41 @@ let State = {
         ],
           
         FriendsData:[
-            { id: 1, FriendsName: "Vasya", FriendsImage: <img src="./image/imageFriends/1.jpg" /> },
-            { id: 2, FriendsName: "Petya", FriendsImage: <img src="./image/imageFriends/2.jpg" /> },
-            { id: 3, FriendsName: "Sasha", FriendsImage: <img src="./image/imageFriends/3.jpg" /> },
-            { id: 4, FriendsName: "Tanya", FriendsImage: <img src="./image/imageFriends/4.jpg" /> },
-            { id: 5, FriendsName: "Petya", FriendsImage: <img src="./image/imageFriends/5.jpg" /> },
-            { id: 6, FriendsName: "Nastya", FriendsImage: <img src="./image/imageFriends/6.jpg" /> },
-            { id: 7, FriendsName: "Yan", FriendsImage: <img src="./image/imageFriends/7.jpg" /> },
-            { id: 8, FriendsName: "Sergei", FriendsImage: <img src="./image/imageFriends/8.jpg" /> },
-            { id: 9, FriendsName: "Natasha", FriendsImage: <img src="./image/imageFriends/9.jpg" /> }
+            { id: 1, FriendsName: "Vasya", FriendsImage: <img src="./image/imageFriends/1.jpg" alt=''/> },
+            { id: 2, FriendsName: "Petya", FriendsImage: <img src="./image/imageFriends/2.jpg" alt='' /> },
+            { id: 3, FriendsName: "Sasha", FriendsImage: <img src="./image/imageFriends/3.jpg" alt='' /> },
+            { id: 4, FriendsName: "Tanya", FriendsImage: <img src="./image/imageFriends/4.jpg" alt='' /> },
+            { id: 5, FriendsName: "Petya", FriendsImage: <img src="./image/imageFriends/5.jpg" alt='' /> },
+            { id: 6, FriendsName: "Nastya", FriendsImage: <img src="./image/imageFriends/6.jpg" alt='' /> },
+            { id: 7, FriendsName: "Yan", FriendsImage: <img src="./image/imageFriends/7.jpg" alt='' /> },
+            { id: 8, FriendsName: "Sergei", FriendsImage: <img src="./image/imageFriends/8.jpg" alt='' /> },
+            { id: 9, FriendsName: "Natasha", FriendsImage: <img src="./image/imageFriends/9.jpg" alt='' /> }
         ],
 
         FriendsOnlineData:[
-            { id: 5, FriendsName: "Petya", FriendsImage: <img src="./image/imageFriends/5.jpg" /> },
-            { id: 6, FriendsName: "Nastya", FriendsImage: <img src="./image/imageFriends/6.jpg" /> },
-            { id: 7, FriendsName: "Yan", FriendsImage: <img src="./image/imageFriends/7.jpg" /> },
-            { id: 8, FriendsName: "Sergei", FriendsImage: <img src="./image/imageFriends/8.jpg" /> },
-            { id: 9, FriendsName: "Natasha", FriendsImage: <img src="./image/imageFriends/9.jpg" /> },
-            { id: 1, FriendsName: "Vasya", FriendsImage: <img src="./image/imageFriends/1.jpg" /> },
-            { id: 2, FriendsName: "Petya", FriendsImage: <img src="./image/imageFriends/2.jpg" /> },
-            { id: 3, FriendsName: "Sasha", FriendsImage: <img src="./image/imageFriends/3.jpg" /> },
-            { id: 4, FriendsName: "Tanya", FriendsImage: <img src="./image/imageFriends/4.jpg" /> }
+            { id: 5, FriendsName: "Petya", FriendsImage: <img src="./image/imageFriends/5.jpg" alt='' /> },
+            { id: 6, FriendsName: "Nastya", FriendsImage: <img src="./image/imageFriends/6.jpg" alt='' /> },
+            { id: 7, FriendsName: "Yan", FriendsImage: <img src="./image/imageFriends/7.jpg" alt='' /> },
+            { id: 8, FriendsName: "Sergei", FriendsImage: <img src="./image/imageFriends/8.jpg" alt='' /> },
+            { id: 9, FriendsName: "Natasha", FriendsImage: <img src="./image/imageFriends/9.jpg" alt='' /> },
+            { id: 1, FriendsName: "Vasya", FriendsImage: <img src="./image/imageFriends/1.jpg" alt='' /> },
+            { id: 2, FriendsName: "Petya", FriendsImage: <img src="./image/imageFriends/2.jpg" alt='' /> },
+            { id: 3, FriendsName: "Sasha", FriendsImage: <img src="./image/imageFriends/3.jpg" alt='' /> },
+            { id: 4, FriendsName: "Tanya", FriendsImage: <img src="./image/imageFriends/4.jpg" alt='' /> }
         ]
   },
 
       MessagePage:{
         UserData: [
-            { id: 7, name: "Yan", Lastname: "Ivanov", UserImage: <img src="./image/imageUser/7.jpg" /> },
-            { id: 8, name: "Sergei", Lastname: "Petrov", UserImage: <img src="./image/imageUser/8.jpg" /> },
-            { id: 9, name: "Natasha", Lastname: "Sidorov", UserImage: <img src="./image/imageUser/9.jpg" /> },
-            { id: 4, name: "Tanya", Lastname: "Smith", UserImage: <img src="./image/imageUser/4.jpg" /> },
-            { id: 5, name: "Petya", Lastname: "Ksavier", UserImage: <img src="./image/imageUser/5.jpg" /> },
-            { id: 6, name: "Nastya", Lastname: "Kipelov", UserImage: <img src="./image/imageUser/6.jpg" /> },
-            { id: 1, name: "Vasya", Lastname: "Bener", UserImage: <img src="./image/imageUser/1.jpg" /> },
-            { id: 2, name: "Petya", Lastname: "Stark", UserImage: <img src="./image/imageUser/2.jpg" /> },
-            { id: 3, name: "Sasha", Lastname: "Mask", UserImage: <img src="./image/imageUser/3.jpg" /> }
+            { id: 7, name: "Yan", Lastname: "Ivanov", UserImage: <img src="./image/imageUser/7.jpg" alt='' /> },
+            { id: 8, name: "Sergei", Lastname: "Petrov", UserImage: <img src="./image/imageUser/8.jpg" alt='' /> },
+            { id: 9, name: "Natasha", Lastname: "Sidorov", UserImage: <img src="./image/imageUser/9.jpg" alt='' /> },
+            { id: 4, name: "Tanya", Lastname: "Smith", UserImage: <img src="./image/imageUser/4.jpg" alt='' /> },
+            { id: 5, name: "Petya", Lastname: "Ksavier", UserImage: <img src="./image/imageUser/5.jpg" alt='' /> },
+            { id: 6, name: "Nastya", Lastname: "Kipelov", UserImage: <img src="./image/imageUser/6.jpg" alt='' /> },
+            { id: 1, name: "Vasya", Lastname: "Bener", UserImage: <img src="./image/imageUser/1.jpg" alt='' /> },
+            { id: 2, name: "Petya", Lastname: "Stark", UserImage: <img src="./image/imageUser/2.jpg" alt='' /> },
+            { id: 3, name: "Sasha", Lastname: "Mask", UserImage: <img src="./image/imageUser/3.jpg" alt='' /> }
           ],
         MessageData: [
             { id: 1, Message: "Hi" },
